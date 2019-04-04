02e736f2bed82054583663a6fdda5871124865ce
neatsdude <niteshdudhe13@gmail.com>
2019-04-04 10:47:50 +0530

    Update operation_01.py
    Sample text  from other code to be edited..later

--- operation_01.py
+++ operation_01.py
@@ -37,6 +37,24 @@ done
 
 
 
+url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+
+names = json.loads(open('names.json').read())
+
+for name in names:
+	name_extra = ''.join(random.choice(string.digits))
+
+	username = name.lower() + name_extra + '@yahoo.com'
+	password = ''.join(random.choice(chars) for i in range(8))
+
+	requests.post(url, allow_redirects=False, data={
+		'auid2yjauysd2uasdasdasd': username,
+		'kjauysd6sAJSDhyui2yasd': password
+	})
+
+	print 'sending username %s and password %s' % (username, password)
+
+
 
 #-----Boiler Plate-----
 if __name__=='__main__':
