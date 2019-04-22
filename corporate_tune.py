6e01c2308a4ac17ba43d23674a4a387ba8998726
neatsdude <niteshdudhe13@gmail.com>
2019-04-22 10:37:40 +0530

    Update corporate_tune.py
    Another interactive version...in progress.

--- corporate_tune.py
+++ corporate_tune.py
@@ -1,84 +1,70 @@
 #!/usr/bin/python3  -tt
 
 import os
+import sys
 import requests
 import mysql.connector
 
-def get_msisdn_info(field):
+# Global variable   url = 'http://172.27.102.56:8080/rbt/rbt_promotion.jsp?MSISDN=701890509&REQUEST=STATUS'
 
-    data_id = os.popen('cut -d, -f%d Base.txt'%(field+1)).readlines()
-    for i in range(len(data_id)):
-        data_id[i] = data_id[i].rstrip('\n')
-    return data_id
-
-def check_status():
-# checking status for each number
-    
-    for i in data_list['charge_id']:
-
-        for_status = (
-            ('MSISDN', '%s') %i,
-            ('REQUEST', 'STATUS')
-                 )
+url = '''http://172.27.102.56:8080/rbt/rbt_promotion.jsp'''
 
 
 def main():
+    print('''Make sure the text file submitted is in this format:
 
-# Getting data from Base.txt in format
-# CHARGING MSISDN, CHILD MSISDN, SUBSCRIPTION CLASS, CHARGE CLASS, CLIP ID, PRE RBT
-# 752122295,757383846,CORP,CORP,12345,YES
+CHARGING MSISDN,CHILD MSISDN,SUBSCRIPTION CLASS,CHARGE CLASS,CLIP ID,PRE RBT
+i.e. No spaces after comma
+752122295,757383846,CORP,CORP,12345,YES''')
+    p1 = input('Press Y to continue or press ctrl + c to exit \n')
+    if p1 == 'Y':
+        file = open(sys.argv[1] , 'r+')
+        lines = file.readlines()
+        file.close()
+        for i in range(len(lines)):
+            lines[i] = lines[i].rstrip('\n')
+            lines[i] = lines[i].split(',')
+        delay = []
+        print('The Charging MSISDN is:' + lines[0][0])
+        print('The Child MSISDN are :')
+        for i in lines:
+            params = (('MSISDN' , i[1]), ('REQUEST', 'STATUS'))
+            response = requests.get(url , params = params)
+            if response.text == 'ACTIVE':
+                delay.append(i[1])
+            print('MSISDN '+ i[1] +' is ' + response.text)
+            if len(delay) == 0:
+                print('All are new numbers')
+            else:
+                print('These numbers are active :' + str(delay) + ' \n')
+        if len(delay) == 0:
+            p2 = input('Procede for Charging Press Y\n')
+            if p2 =='Y':
+                    params = (
+                            ('MSISDN', '702746048'),
+                            ('REQUEST', 'selection'),
+                            ('SELECTED_BY', 'CCC'),
+                            ('TONE_ID', '31121745'),
+                            ('CATEGORY_ID', '1'),
+                            ('ISACTIVATE', 'TRUE'),
+                            ('CHARGEMDN', '702475655'),
+                            ('CHARGE_CLASS', 'CORP3'),
+                            ('SUBSCRIPTION_CLASS', 'CORP3'),
+                            ('SELECTIONTYPE', '2'))
 
-# Make sure the Base.txt is in the same directory as the script
-    data_list = {'charge_id': [], 'child_id': [], 'subs_class': [], 'charge_class': [], 'clip_id': [], 'pre_rbt': []}
-    keys = list(data_list)
-    for num in range(len(data_list)):
-        data_list[keys[num]] = get_msisdn_info(num)
+        p2 = input('To Deactivate Press D\n')
+        if p2 == 'D':
+            for i in delay:
+                params = (('MSISDN', i),('REQUEST', 'DEACTIVATE'),('DEACTIVATED_BY', 'CCC'))
+                dct_resp = requests.get(url, params = params)
+        else:
+            print('Exiting..!!')
 
-
-
-
-
-
-# url = 'http://172.27.102.56:8080/rbt/rbt_promotion.jsp?MSISDN=701890509&REQUEST=STATUS'
-
-    url = '''http://172.27.102.56:8080/rbt/rbt_promotion.jsp'''
-
-    for_activation = (
-        ('MSISDN', '702746048'),
-        ('REQUEST', 'selection'),
-        ('SELECTED_BY', 'CCC'),
-        ('TONE_ID', '31121745'),
-        ('CATEGORY_ID', '1'),
-        ('ISACTIVATE', 'TRUE'),
-        ('CHARGEMDN', '702475655'),
-        ('CHARGE_CLASS', 'CORP3'),
-        ('SUBSCRIPTION_CLASS', 'CORP3'),
-        ('SELECTIONTYPE', '2'),
-                  )
-
-    for_deactivation = (
-        ('MSISDN', '$f[0]'),
-        ('REQUEST', 'DEACTIVATE'),
-        ('DEACTIVATED_BY', 'CCC'),
-                    )
-
-    stat_res = requests.get(url , params=for_status)
-    act_res = requests.get(url , params=for_activation)
-
-# List of responses
-
-    response_list = ['ACTIVE','DEACTIVE','SUBSCRIBER_NOT_EXIST','NEW_USER','GRACE','SUSPENDED','GRACE','ACT_PENDING','DEACT_PENDING','SUSPENDED','INVALID','ACT_PENDING'
-,'DEACT_PENDING']
-
-    if stat_res.text in 'ACTIVE':
-
-        for_deactivation = (
-        ('MSISDN', '%s')%child_id,
-        ('REQUEST', 'DEACTIVATE'),
-        ('DEACTIVATED_BY', 'CCC'),
-                    )
-
-        deact_res = requests.get(url , params=for_deactivation)
+    else:
+        print('''You have entered wrong value
+Exiting the program...''')
 
 
 
+if __name__ == '__main__':
+    main()
